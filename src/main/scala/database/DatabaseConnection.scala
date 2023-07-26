c3912bd3ac3e876f3a7aca02a1cbc8c90c257baf
Sagar Goel <sagar@ebctechnologies.com>
2023-07-26 16:53:07 +0530

    Worked with Database Connection

--- src/main/scala/database/DatabaseConnection.scala
+++ src/main/scala/database/DatabaseConnection.scala
@@ -3,26 +3,33 @@ package database
 import java.sql.{Connection, DriverManager}
 
 object DatabaseConnection {
-  val url = "jdbc:mysql://localhost/fraud_detector_database"
-  val driver = "com.mysql.cj.jdbc.Driver"
-  val username = "root"
-  val password = "password"
-  var connection: Connection = _
+  // Database connection parameters
+  private val url = "jdbc:mysql://localhost/fraud_detector_database"
+  private val driver = "com.mysql.cj.jdbc.Driver"
+  private val username = "root"
+  private val password = "password"
+  private var connection: Connection = _
 
+  /**
+   * Get a connection to the database.
+   *
+   * @return A connection to the database.
+   */
   def getConnection(): Connection = {
     if (connection != null) {
+      // If a connection already exists, return it
       connection
-    }
-    else {
+    } else {
       try {
+        // Load the JDBC driver and create a new connection
         Class.forName(driver)
         connection = DriverManager.getConnection(url, username, password)
         connection
       } catch {
-        case e: Exception => e.printStackTrace
+        case e: Exception =>
+          e.printStackTrace()
           null
       }
     }
   }
-
 }
