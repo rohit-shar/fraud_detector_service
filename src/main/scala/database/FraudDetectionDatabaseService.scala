8d32e92f16449dcd36ab75cef87ac542aa2644d7
Rohit <rohit.sharma@spineor.com>
2023-07-26 17:15:23 +0530

    Created "GETTING FRAUD FROM DATABASE" functionality

--- src/main/scala/database/FraudDetectionDatabaseService.scala
+++ src/main/scala/database/FraudDetectionDatabaseService.scala
@@ -72,6 +72,7 @@ class FraudDetectionDatabaseService {
   // Below methode is used for updating sift status and sift score of existing fraud record
   def updateExistingFraudEntryRecord(orderNumber: String, fraudResponse: FraudResponse): Int = {
     try {
+      val connection = getConnection()
       val preparedStatement = connection.prepareStatement(FRAUD_RECORD_SIFT_STATUS_UPDATE_QUERY)
       preparedStatement.setDouble(1, fraudResponse.avg_score)
       preparedStatement.setString(2, fraudResponse.orderStatus)
